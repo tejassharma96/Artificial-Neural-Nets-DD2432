8fce82297579f27ba8e139f1ba608908adef2def
Tejas Sharma <tejas.s1996@gmail.com>
2017-01-18 16:14:53 +0100

    added generation code to sepdata

---
+++ Lab 1/sepdata.m
@@ -0,0 +1,14 @@
+% generate data
+classA(1,:) = randn(1,100) .* 0.5 + 1.0;
+classA(2,:) = randn(1,100) .* 0.5 + 0.5;
+classB(1,:) = randn(1,100) .* 0.5 - 1.0;
+classB(2,:) = randn(1,100) .* 0.5 + 0.0;
+
+% organise patterns and targets
+patterns = [classA, classB]
+targets = [ones(1,100), -ones(1,100)]
+
+% permute randomly
+permute = randperm(200)
+patterns = patterns(:, permute)
+targets = targets(:, permute)
