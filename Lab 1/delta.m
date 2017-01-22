fe03349568de9ebfabe490975017a79eee812fa4
Tejas Sharma <tejas.s1996@gmail.com>
2017-01-22 20:50:07 +0100

    finished delta rule

--- Lab 1/delta.m
+++ Lab 1/delta.m
@@ -3,12 +3,35 @@
 % learning factor should be suitably small: 0.001
 
 clear;
-sepdata;
+nsepdata;
 learning_factor = 0.001
 epochs = 20
-% set up X so that it has the extra layer of 1s for the bias
-X = [patterns; ones(1, ndata)];
 
 % get dimensions
 [insize, ndata] = size(patterns)
 [outsize, ndata] = size(targets)
+
+% set up X so that it has the extra layer of 1s for the bias
+X = [patterns; ones(1, ndata)];
+
+% generate weights
+W = randn(1, insize + 1);
+
+% perform delta rule
+for i = 0:epochs
+    W -= learning_factor * (W*X - targets)*X';
+
+    % graph each epoch
+    p = W(1, 1:2);
+    k = -W(1, insize+1) / (p*p');
+    l = sqrt(p*p');
+    axis ([-2, 2, -2, 2], 'square')
+    plot(patterns(1, find(targets>0)), ...
+        patterns(2, find(targets>0)), '*', ...
+        patterns(1, find(targets<0)), ...
+        patterns(2, find(targets<0)), '+', ...
+        [p(1), p(1)]*k + [-p(2), p(2)]/l, ...
+        [p(2), p(2)]*k + [p(1), -p(1)]/l, '-');
+    drawnow;
+
+end
