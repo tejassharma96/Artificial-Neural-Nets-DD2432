408c20686cede80a13049fe7a263648ab264a289
Tejas Sharma <tejas.s1996@gmail.com>
2017-02-15 10:55:01 +0100

    :mps sort of working

--- Lab 3/task5_mps.m
+++ Lab 3/task5_mps.m
@@ -7,28 +7,29 @@ ypos = reshape(y, 1, 100);
 votes;
 mpparty;
 mpsex;
+mpdistrict;
 eta = 0.2;
 epochs = 50;
 pos = ones(1, 349);
 
-w = rand(100, 31)
+w = rand(100, 31);
 party = zeros(100, 8);
 
 for i = 1:epochs
     % outer loop
 
-    for a = 1:349
-        p = votes(a, :);
-        dist = (ones(100, 1) * p - w);
+    for a = randperm(349)
+        mp = votes(a, :);
+        dist = (ones(100, 1) * mp - w);
         dist_squared = sum(dist.^2, 2);
         [val index] = min(dist_squared);
 
         n = 3 - 3*(i/epochs);
 
         for ind = 1:100
-            d = abs(xpos(ind) - xpos(index)) + abs(ypos(ind) - ypos(index))
+            d = abs(xpos(ind) - xpos(index)) + abs(ypos(ind) - ypos(index));
             if d <= n
-                w(ind, :) = w(ind, :) + eta*(p - w(ind, :));
+                w(ind, :) = w(ind, :) + eta*(mp - w(ind, :));
             endif
         endfor
         pos(a) = index;
@@ -40,7 +41,6 @@ endfor
 a = ones(1, 100) * 350;
 a(pos) = 1:349;
 
-colormap([0 0 0; 0 0 1; 0 1 1; 1 0 1; 1 0 0; 0 1 0; 1 1 1; 1 1 0])
 
 % Display
 b = zeros(1, 100);
@@ -50,10 +50,21 @@ for i = 1:100
         b(i) = posMax;
     endif
 endfor
+
 figure(1);
+title('Most common party per unit');
 p = [mpparty;0];
+colormap([0 0 0; 0 0 1; 0 1 1; 1 0 1; 1 0 0; 0 1 0; 1 1 1; 1 1 0])
 image(p(reshape(a,10,10))+1);
 
+figure(2);
+title('Most common district per unit');
+d = [mpdistrict;0];
+colormap([0 0 0; 0 0 1; 0 1 1; 1 0 1; 1 0 0; 0 1 0; 1 1 1; 1 1 0])
+image(d(reshape(a,10,10))+1);
+
 figure(3);
+title('Most common sex per unit');
 s = [mpsex;0];
+colormap([0 0 0; 0 0 1; 0 1 1; 1 0 1; 1 0 0; 0 1 0; 1 1 1; 1 1 0])
 image(s(reshape(a,10,10))+1);
