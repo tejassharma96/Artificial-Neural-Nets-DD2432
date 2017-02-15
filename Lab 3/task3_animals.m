99bdfd0178b7c656f7f64b88776c073f217d7890
Tejas Sharma <tejas.s1996@gmail.com>
2017-02-15 12:19:55 +0100

    added videos

--- Lab 3/task3_animals.m
+++ Lab 3/task3_animals.m
@@ -2,7 +2,8 @@ clear;
 close all;
 animals;
 
-w = rand(100, 84);
+out_nodes = 20;
+w = rand(out_nodes, 84);
 eta = 0.2;
 epochs = 40;
 
@@ -10,7 +11,7 @@ for i = 1:epochs
     % outer loop
     for a = 1:32
         p = props(a, :);
-        dist = (ones(100, 1) * p - w);
+        dist = (ones(out_nodes, 1) * p - w);
         dist_squared = sum(dist.^2, 2);
         [val index] = min(dist_squared);
 
@@ -20,8 +21,8 @@ for i = 1:epochs
         if min_index < 1
             min_index = 1;
         end
-        if max_index > 100
-            max_index = 100;
+        if max_index > out_nodes
+            max_index = out_nodes;
         end
         for ind = min_index:max_index
             w(ind, :) = w(ind, :) + eta*(p - w(ind, :));
@@ -33,7 +34,7 @@ end
 pos = zeros(32, 1);
 for a = 1:32
     p = props(a,:);
-    dist = (ones(100,1) * p - w);
+    dist = (ones(out_nodes,1) * p - w);
     dist_squared = sum(dist.^2, 2);
     [val index] = min(dist_squared);
     pos(a) = index;
