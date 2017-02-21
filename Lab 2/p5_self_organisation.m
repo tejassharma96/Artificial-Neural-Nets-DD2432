a0193c5f2725a976121532f9715a5d022e455e20
Tejas Sharma <tejas.s1996@gmail.com>
2017-02-21 15:49:22 +0100

    p5 done

--- Lab 2/p5_self_organisation.m
+++ Lab 2/p5_self_organisation.m
@@ -1,7 +1,38 @@
 clear;
+close all;
 
 plotinit;
 data = read('cluster');
 units = 5;
 vqinit;
 singlewinner = 1;
+title('CL - Single Winner');
+vqiter;
+
+
+clear;
+plotinit;
+data = read('cluster');
+units = 5;
+vqinit;
+singlewinner = 0;
+title('CL - All Units');
+vqiter;
+
+clear;
+plotinit;
+data = read('cluster');
+units = 5;
+vqinit;
+singlewinner = 1;
+title('EM - Single Winner');
+emiterb;
+
+clear;
+plotinit;
+data = read('cluster');
+units = 30;
+vqinit;
+singlewinner = 0;
+title('EM - All Units');
+emiterb;
