e17590cc4351b5df9ea150d7caf25c66bc1b3c67
Tejas Sharma <tejas.s1996@gmail.com>
2017-02-21 18:29:39 +0100

    done upto 5.1

--- Lab 4/hebbian_learning.m
+++ Lab 4/hebbian_learning.m
@@ -10,9 +10,9 @@ mat = [x1; x2; x3];
 w = calc_weights(mat);
 
 % run updates
-[x1_updated, ~] = update_weights(w, x1);
-[x2_updated, ~] = update_weights(w, x2);
-[x3_updated, ~] = update_weights(w, x3);
+[x1_updated, ~] = update_patterns(w, x1);
+[x2_updated, ~] = update_patterns(w, x2);
+[x3_updated, ~] = update_patterns(w, x3);
 
 % make sure nothing changed
 isequal(x1, x1_updated)
@@ -26,6 +26,16 @@ x3d=vm([1 1 1 0 1 1 0 1]);
 
 test_patterns = [x1d; x2d; x3d];
 for x = test_patterns'
-    [x_updated, it] = update_weights
+    [x_updated, it] = update_patterns(w, x');
+    fprintf('Got %s from %s in %d iterations\n', num2str(t0(x_updated)), num2str(t0(x')), it);
 end
 
+% success!
+% number of attractors
+attractors = mat;
+for i = permn([0 1], 8)'
+    [x_updated, ~] = update_patterns(w, vm(i)');
+    if not(ismember(x_updated, attractors, 'rows'))
+        attractors = [attractors; x_updated];
+    end
+end
