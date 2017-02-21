c8cfc4217234932be4dd2b484841cb2029a735ea
Tejas Sharma <tejas.s1996@gmail.com>
2017-02-21 18:39:48 +0100

    normalize weights option

--- Lab 4/calc_weights.m
+++ Lab 4/calc_weights.m
@@ -1,9 +1,15 @@
-function [w] = calc_weights(patterns, correct_diagonal)
+function [w] = calc_weights(patterns, normalize, correct_diagonal)
+    if nargin < 3
+        normalize = 0;
+    end
     if nargin < 2
         correct_diagonal = 1;
     end
-    [~, N] = size(patterns);
-    w = patterns' * patterns ./ N;
+    w = patterns' * patterns;
+    if normalize
+        [~, N] = size(patterns);
+        w = w ./ N;
+    end
     if correct_diagonal
         w = w - diag(diag(w));
     end
