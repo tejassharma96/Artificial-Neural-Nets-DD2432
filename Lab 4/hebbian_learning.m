334bcd33459832fe186331fc0ef1b20eb65d5363
Tejas Sharma <tejas.s1996@gmail.com>
2017-02-21 18:57:53 +0100

    kill me pls

--- Lab 4/hebbian_learning.m
+++ Lab 4/hebbian_learning.m
@@ -7,7 +7,7 @@ x3 = vm([0 1 1 0 0 1 0 1]);
 mat = [x1; x2; x3];
 
 % calculate weights
-w = calc_weights(mat);
+w = calc_weights(mat, 0, 0);
 
 % run updates
 [x1_updated, ~] = update_patterns(w, x1);
@@ -15,14 +15,16 @@ w = calc_weights(mat);
 [x3_updated, ~] = update_patterns(w, x3);
 
 % make sure nothing changed
-isequal(x1, x1_updated)
-isequal(x2, x2_updated)
-isequal(x3, x3_updated)
+isequal(x1, x1_updated);
+isequal(x2, x2_updated);
+isequal(x3, x3_updated);
 
 % see if memory can recall from distorted input patterns
 x1d=vm([1 0 1 0 1 0 0 1]);
 x2d=vm([1 1 0 0 0 1 0 0]);
 x3d=vm([1 1 1 0 1 1 0 1]);
+x4d = vm([0 0 1 0 1 0 0 1]);
+x5d = vm([0 0 1 0 1 0 0 1]);
 
 test_patterns = [x1d; x2d; x3d];
 for x = test_patterns'
