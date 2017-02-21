5d7dec7492cce24b5dbf4566f852ccf5bd97b9be
Tejas Sharma <tejas.s1996@gmail.com>
2017-02-21 18:37:23 +0100

    option to correct diagonal in calc_weights by default

--- Lab 4/calc_weights.m
+++ Lab 4/calc_weights.m
@@ -1,4 +1,11 @@
-function [w] = calc_weights(patterns)
-    w = patterns' * patterns;
+function [w] = calc_weights(patterns, correct_diagonal)
+    if nargin < 2
+        correct_diagonal = 1;
+    end
+    [~, N] = size(patterns);
+    w = patterns' * patterns ./ N;
+    if correct_diagonal
+        w = w - diag(diag(w));
+    end
 end
 
